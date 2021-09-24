ebf4b87825de5365031cdc93ddefad2976352ecd
Lalluviadel <inspiracion@yandex.ru>
2021-09-24 22:05:36 +0300

    During the lesson_04: - preparation login and use form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from users.forms import userLoginForm
 
-# Create your views here.
+
+def login(request):
+    form = userLoginForm()
+    context = {
+        'title': 'Geekshop - Авторизация',
+        'form': form,
+    }
+    return render(request,'users/login.html',context)
+
+def register(request):
+    context = {
+        'title': 'Geekshop - Регистрация',
+    }
+    return render(request,'users/register.html',context)
