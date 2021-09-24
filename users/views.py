9955096c6be1820cbc97de2a36df67b8d3f69a89
Lalluviadel <inspiracion@yandex.ru>
2021-09-25 01:30:43 +0300

    During the lesson_04: - added login, logout and register

--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from users.forms import userLoginForm
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm,UserRegisterForm
 
 
 def login(request):
-    form = userLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,password=password)
+            if user.is_active:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'Geekshop - Авторизация',
         'form': form,
@@ -11,7 +25,21 @@ def login(request):
     return render(request,'users/login.html',context)
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
     context = {
         'title': 'Geekshop - Регистрация',
+        'form':form,
     }
     return render(request,'users/register.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
