88e4df5a89602be6c17e32c1dd6940f771f87d21
Lalluviadel <inspiracion@yandex.ru>
2021-09-28 20:26:17 +0300

    During the lesson_05: - added app baskets

---
+++ baskets/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+app_name = 'baskets'
+# urlpatterns = [
+#     path('login/', login, name='login'),
+#     path('register/', register, name='register'),
+#     path('profile/', profile, name='profile'),
+#     path('logout/', logout, name='logout'),
+# ]
