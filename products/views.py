cb7feaab54691d7bf7e0741eb204b5dbf83f5983
Lalluviadel <inspiracion@yandex.ru>
2021-10-07 12:09:17 +0300

    During the Lesson_08: - added paginator and template filter for categories and products;

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Product,ProductCategory
+
 
 def index(request):
     context = {
@@ -9,9 +11,21 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    title = 'geekshop'
-    categories = ProductCategory.objects.all()
-    product = Product.objects.all()
-    content = {'title': title, 'products': product, 'category': categories}
-    return render(request, 'products/products.html', content)
+def products(request, category_id=None, page_id=1):
+    products = Product.objects.filter(category_id=category_id) \
+        if category_id is not None else Product.objects.all()
+    paginator = Paginator(products, per_page=3)
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context = {
+        'title': 'Каталог',
+        'categorys': ProductCategory.objects.all(),
+        'products': products_paginator,
+        }
+    # Lesson 8: дополнить/изменить значение по ключу в словаре context можно и так:
+    # context.update({'products': products_paginator})
+    return render(request, 'products/products.html', context)
